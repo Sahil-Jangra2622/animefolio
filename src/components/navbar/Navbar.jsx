7922fc9268ba418ce99e8c91eef554773f6436c0
sahil <sahiljangra2622@gmail.com>
2023-11-11 03:21:48 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Sahil Jangra
+        </motion.span>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/sahil-jangra-13a5b2231/" target="_blank">
+            <img src="/linkedin.png" alt="" />
+          </a>
+          <a href="https://leetcode.com/Sahil_Jangra/" target="blank">
+          <img src="/leetcode.png" alt="" style={{height:"30px" , width:"30px"}}/>
+          </a>
+          <a href="https://github.com/Sahil-Jangra2622" target="blank">
+            <img src="/git.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,85 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Netflix Clone",
+    img: "https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png",
+    desc: "Created a Subscription video on-demand over the-top streaming service working in a team of 2.Provides Anime series, films, television shows produced by all companies.Tech used – HTML, CSS, JAVASCRIPT, REACTJS, NODEJS, EXPRESSJS, REDUX, API and MONGODB",
+    link:"https://netflix-mern-project.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "Daily Doze",
+    img: "https://static.vecteezy.com/system/resources/thumbnails/008/471/381/small/social-live-stream-linear-icon-web-streaming-badge-online-broadcast-news-thin-line-contour-button-symbol-isolated-outline-illustration-png.png",
+    desc: "Application which offer the instant & latest news about public, political, social, sports events.Tech used — HTML, CSS, JAVASCRIPT, BOOTSTRAP, REACTJS, and API",
+    link:"https://daily-doze.netlify.app/"
+  },
+  {
+    id: 3,
+    title: "I Notebook",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFDqghdWbgZSBHvlmo-ZoNpsUvtR3I66rDv1q9lAB5nB6Rlw4455mk8UqjJWZrIUXl2-w&usqp=CAU",
+    desc: "A short notes app can be used for quick jottings, to-do lists, shopping lists, meeting notes, bookmarking, brainstorming, journaling, storing passwords, collaboration, and organization. ",
+    link:"https://main--my-inotebooks.netlify.app/"
+  },
+
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <a href={item.link} alt="" target="blank">
+            <button>See Demo</button>
+            </a>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
